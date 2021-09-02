a2ced31a2b395ec8e2e144beefc3b564c6489083
Sumonbhuiya <sumonbhuiya97@gmail.com>
2021-09-02 13:49:09 +0600

    finish

--- js/book.js
+++ js/book.js
@@ -1,3 +1,4 @@
+// search for fiending book 
 const searchBook = () => {
     const getSearch = document.getElementById('search-field');
     const getText = getSearch.value;
@@ -15,36 +16,38 @@ const searchBook = () => {
         const url = `http://openlibrary.org/search.json?q=${getText}`;
         fetch(url)
             .then(res => res.json())
-            .then(json => displaySearchResult(json.docs));
+            .then(json => displaySearchResult(json));
     }
 }
-const displaySearchResult = docs => {
+// find search book function 
+const displaySearchResult = result => {
     // get found result or not
     const bookDetails = document.getElementById('book-details');
     bookDetails.innerText = '';
     const h5 = document.createElement('h5');
-    if (docs.length !== 0) {
-        h5.innerText = `Total result found: ${docs.length}`;
+    if (result.numFound !== 0) {
+        h5.innerText = `Total result found: ${result.numFound}`;
     } else {
         h5.innerText = `Result not found`;
     }
     bookDetails.appendChild(h5);
     // display search books which are found 
+    const books = result.docs;
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    // display 9 results 
-    docs.slice(0, 6).forEach(book => {
+    // display 15 results using HTML  
+    books.slice(0, 15).forEach(book => {
         const pictureUrl = `https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
             <div class="card-body">
-                <img src="${pictureUrl}" class="card-img-top" style="height:60%" alt="">
-                <div class="card-footer mt-5">
+                <img src="${pictureUrl}" class="card-img-top" style="height:400px" alt="">
+                <div class="card-footer mt-3">
                 <h5 class="card-text">Name: <span class="text-primary">${book.title ? book.title : ''}</span></h5>
-                <h6 class="card-text">Writer: <span class="text-success">${book.author_name[0] ? book.author_name[0] : ''}</span></h6>
-                <h6 class="card-text">Publisher: <span class="text-success">${book.publisher[0] ? book.publisher[0] : ''}</span></h6>
+                <h6 class="card-text">Writer: <span class="text-success">${book.author_name ? book.author_name : ''}</span></h6>
+                <h6 class="card-text">Publisher: <span class="text-success">${book.publisher ? book.publisher : ''}</span></h6>
                 <h6 class="card-text">First Publish: <span class="text-info">${book.first_publish_year ? book.first_publish_year : ''}</span></h6>
                 </div>
             </div>
